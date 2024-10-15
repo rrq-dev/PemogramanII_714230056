302397c22c0608cf87e896947b2d622ef23d8ed8
rrq-dev <raihanhermawan85@gmail.com>
2024-10-15 16:17:23 +0700

    praktikum ke 2

---
+++ Praktikum/P2_1_714230056/P2 Pemograman II/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2_Pemograman_II
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double num1, num2;
+
+            // Input angka pertama
+            Console.Write("Masukkan angka pertama: ");
+            num1 = Convert.ToDouble(Console.ReadLine());
+
+            // Input angka kedua
+            Console.Write("Masukkan angka kedua: ");
+            num2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine(num1 + " + " + num2 + " = " + (num1 + num2));
+            Console.WriteLine(num1 + " - " + num2 + " = " + (num1 - num2));
+            Console.WriteLine(num1 + " * " + num2 + " = " + (num1 * num2));
+            Console.WriteLine(num1 + " / " + num2 + " = " + (num1 / num2));
+
+
+
+
+
+        }
+    }
+}
